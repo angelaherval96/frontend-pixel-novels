27dcf62e671f439197c603af77a84b2a6bd5e2b4
Ángela Hernández <angelaherval96@digitechfp.com>
2025-05-26 23:27:10 +0200

    Creación de rutas
    Enlaces a las páginas principales.
    Paleta de colores en archivo variables.css.
    Creación del componente Footerbar

---
+++ src/views/novels/NovelsLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Gestión de Novelas</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+
+    <IonContent>
+      <router-view />
+    </IonContent>
+  </IonPage>
+</template>
+
+<script setup>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue'
+</script>
+
+<style scoped>
+/* Estilos opcionales */
+</style>
---
+++ src/views/profile/ProfileLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Mi Perfil</IonTitle>
+        <!-- Ejemplo: botón de logout -->
+            <IonButtons slot="end">
+                <IonButton>
+                    <IonIcon :icon="logOutOutline" />
+                </IonButton>
+            </IonButtons>
+      </IonToolbar>
+    </IonHeader>
+
+    <IonContent>
+      <router-view />
+    </IonContent>
+  </IonPage>
+</template>
+
+<script setup>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue'
+</script>
+
+<style scoped>
+/* Estilos específicos del perfil */
+</style>
